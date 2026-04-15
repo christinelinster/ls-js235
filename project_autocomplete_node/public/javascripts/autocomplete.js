6d70cda07ac70180cd8d316c03c35075cf70e0d8
Christine Lin <lin.christine19@gmail.com>
2026-04-15 09:41:58 -0400

    add: handle keydown and mousedown events

--- project_autocomplete_node/public/javascripts/autocomplete.js
+++ project_autocomplete_node/public/javascripts/autocomplete.js
@@ -7,16 +7,71 @@ const Autocomplete = {
   },
 
   bindEvents: function(){
-    this.input.addEventListener('input', this.valueChanged.bind(this))
+    this.input.addEventListener('input', this.valueChanged.bind(this));
+    this.input.addEventListener('keydown', this.handleKeydown.bind(this));
+    this.listUI.addEventListener('mousedown', this.handleMousedown.bind(this))
+  },
+
+  handleKeydown: function(event) {
+    switch(event.key) {
+      case 'ArrowDown':
+        event.preventDefault();
+        if (this.selectedIndex === null || this.selectedIndex === this.matches.length - 1) {
+          this.selectedIndex = 0;
+        } else {
+          this.selectedIndex += 1;
+        }
+        this.bestMatchIndex = null;
+        this.draw();
+        break;
+
+      case 'ArrowUp':
+        event.preventDefault();
+        if (this.selectedIndex === null || this.selectedIndex === 0){
+          this.selectedIndex = this.matches.length - 1;
+        } else {
+          this.selectedIndex -= 1;
+        }
+        this.bestMatchIndex = null;
+        this.draw();
+        break;
+
+      case 'Tab':
+        if (this.bestMatchIndex !== null && this.matches.length !== 0) {
+          this.input.value = this.matches[this.bestMatchIndex].name;
+          event.preventDefault();
+        }
+        this.reset();
+        break;
+
+      case 'Enter':
+        this.reset();
+        break;
+
+      case 'Escape':
+        this.input.value = this.previousValue;
+        this.reset();
+        break;
+    }
+
+
+  },
+
+  handleMousedown: function(event) {
+    let element = event.target;
+    this.input.value = element.textContent;
+    this.reset();
   },
 
   valueChanged: async function(){
     let value = this.input.value;
+    this.previousValue = value;
 
     if (value.length > 0) {
       this.matches = await this.fetchMatches(value)
       this.visible = true;
       this.bestMatchIndex = 0;
+      this.selectedIndex = null;
       this.draw();
     } else {
       this.reset();
@@ -46,9 +101,14 @@ const Autocomplete = {
       this.overlay.textContent = ''
     }
 
-    this.matches.forEach(match => {
+    this.matches.forEach( (match, index) => {
       let li = document.createElement('li')
       li.classList.add('autocomplete-ui-choice')
+
+      if (index === this.selectedIndex) {
+        li.classList.add('selected')
+        this.input.value = match.name;
+      }
 
       li.textContent = match.name;
       this.listUI.appendChild(li);
@@ -64,6 +124,9 @@ const Autocomplete = {
     this.visible = false;
     this.matches = [];
     this.bestMatchIndex = null;
+    this.selectedIndex = null;
+    this.previousValue = null;
+
     this.draw();
   },
 
@@ -91,6 +154,8 @@ const Autocomplete = {
     this.visible = false;
     this.matches = [];
     this.bestMatchIndex = null;
+    this.selectedIndex = null;
+    this.previousValue = null;
 
     this.wrapInput();
     this.createUI();
